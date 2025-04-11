aac46a7ded74a6019805939136e7aa50829192be
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-04-12 00:58:05 +0300

    Layout Component completed

---
+++ Frontends/MovieApi.WebUI/Controllers/MovieControllercs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class MovieControllercs : Controller
+    {
+        public IActionResult MovieList()
+        {
+            return View();
+        }
+    }
+}
